39961dd97ed9e33413627e4a96e062a2837f7281
julioaguillar <julioaguilar@outlook.com.br>
2022-01-28 10:46:43 -0300

    Frontend do projeto Vue.js Electron

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vue.js Electron</v-toolbar-title>
+    </v-app-bar>
+    
+    <v-main>
+      <home></home>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-main fluid>
+        <v-form>
+            <v-file-input 
+                label="Seleciona o arquivo"
+                append-icon="mdi-send"
+                multiple 
+                chips 
+                v-model="files"
+                @click:append="processaArquivo" />
+
+            
+        </v-form>
+        <div class="pills">
+            <pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount"/>
+        </div>
+    </v-main>
+</template>
+
+<script>
+import Pill from './Pill.vue'
+
+export default {
+    components: { Pill },
+
+    data: function() {
+        return {
+            files: [],
+            groupedWords: [
+                { name: 'i', amount: 1234 },
+                { name: 'you', amount: 900 },
+                { name: 'he', amount: 853 },                
+            ]
+
+        }
+    },
+
+    methods: {
+        processaArquivo() {
+            console.log(this.files)
+        }        
+    }
+
+}
+</script>
+
+<style>
+
+    .pills {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+</style>
